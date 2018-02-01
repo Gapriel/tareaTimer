f20bf781d258aa6fc0d61deeaa32e21cd5be9068
Gapriel <ie699356@iteso.mx>
2018-01-31 23:42:51 -0600

    casi compleot
    faltan pequeños detalles (pero no hay tiempo para describirlos aqui por ahora)

--- main.c
+++ main.c
@@ -44,22 +44,46 @@
 #include "fsl_port.h"
 #include "fsl_gpio.h"
 #include "fsl_pit.h"
-#include "NVIC.h"
 
-//type defined by the user to stablish without magic numbers the flow direction using boolean logic
+//type defined by the user to establish without magic numbers the flow direction using boolean logic
 typedef enum {
 	FORWARD, REVERSE
 } flow_direction_t;
 
-static uint8_t flow = FORWARD;	//0 for forward, 1 for reverse
+//type defined by the user to establish the color state
+typedef enum {
+	RED, GREEN, BLUE
+} color_state_t;
 
-//SW3 interrupt service routine
-PORTA_IRQHandler() {
+static uint8_t gFlow = FORWARD;	//0 for forward, 1 for reverse
+static uint8_t gState = RED;	//current led color
+static PIT_Type gPit_0;			//PIT 0 base declaration
+
+void PORTA_IRQHandler() {		//SW3 interrupt service routine
+	PORT_ClearPinsInterruptFlags(PORTA, 1 << 4);//SW3 pin interrupt flag clearing
+	gFlow = (FORWARD == gFlow) ? REVERSE : FORWARD;		//flow variable toggle
+}
+
+void PORTC_IRQHandler() {		//SW2 interrupt service routine
+	PORT_ClearPinsInterruptFlags(PORTC, 1 << 6);//SW2 pin interrupt flag clearing
 
 }
-//SW2 interrupt service routine
-PORTC_IRQHandler() {
 
+void PIT0_IRQHandler() {
+	PIT_ClearStatusFlags(&gPit_0, kPIT_Chnl_0, kPIT_TimerFlag);	//pit0 interrupt flag cleared
+	if (FORWARD == gFlow) {
+		if (BLUE > gState) {	//if the state is smaller than BLUE (2)
+			gState++;	//state increase
+		} else {
+			gState = RED;	//state reset to RED (0)
+		}
+	} else {
+		if (RED < gState) {		//if the state is bigger than RED (0)
+			gState--;	//state increase
+		} else {
+			gState = BLUE;	//state reset to BLUE (2)
+		}
+	}
 }
 
 int main(void) {
@@ -70,15 +94,15 @@ int main(void) {
 	CLOCK_EnableClock(kCLOCK_PortC);
 	CLOCK_EnableClock(kCLOCK_PortE);
 
-	//led gpio ports configuration
+	//led ports configuration
 	port_pin_config_t config_led = { kPORT_PullDisable, kPORT_SlowSlewRate,
 			kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
-			kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnLockRegister, };
+			kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnlockRegister };
 	PORT_SetPinConfig(PORTB, 21, &config_led);	//Blue led configuration
 	PORT_SetPinConfig(PORTB, 22, &config_led);	//Red led configuration
 	PORT_SetPinConfig(PORTE, 26, &config_led);	//Green led configuration
 
-	//switch gpio ports configuration
+	//switch ports configuration
 	port_pin_config_t config_switch = { kPORT_PullDisable, kPORT_SlowSlewRate,
 			kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
 			kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnlockRegister };
@@ -87,8 +111,51 @@ int main(void) {
 	PORT_SetPinInterruptConfig(PORTA, 4, kPORT_InterruptFallingEdge);
 	PORT_SetPinInterruptConfig(PORTC, 6, kPORT_InterruptFallingEdge);
 
-	while (1) {
+	//leds gpio configuration
+	gpio_pin_config_t led_config_gpio = { kGPIO_DigitalOutput, 1 };
+	GPIO_PinInit(GPIOB, 21, &led_config_gpio);
+	GPIO_PinInit(GPIOB, 22, &led_config_gpio);
+	GPIO_PinInit(GPIOE, 26, &led_config_gpio);
 
+	//switch gpio configuration
+	gpio_pin_config_t switch_config_gpio = { kGPIO_DigitalInput, 1 };
+	GPIO_PinInit(GPIOA, 4, &switch_config_gpio);
+	GPIO_PinInit(GPIOC, 6, &switch_config_gpio);
+
+	//PIT timer configuration
+	pit_config_t config_pit;
+	PIT_GetDefaultConfig(&config_pit);
+	PIT_Init(&gPit_0, &config_pit);
+	PIT_SetTimerPeriod(&gPit_0, kPIT_Chnl_0, 21000000);	//pit timer set to interrupt every second
+	PIT_EnableInterrupts(&gPit_0, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+
+	//interrupts configuration
+	NVIC_EnableIRQ(PORTA_IRQn);		//SW3 interrupt enabled
+	NVIC_EnableIRQ(PORTC_IRQn);		//SW2 interrupt enabled
+	NVIC_EnableIRQ(PIT0_IRQn);		//PIT 0 interrupt enabled
+
+	//pit timer enabling
+	PIT_StartTimer(&gPit_0, kPIT_Chnl_0);
+
+	//program super loop
+	for (;;) {
+		switch (gState) {
+		case RED:
+			GPIO_WritePinOutput(GPIOB, 21, 1);
+			GPIO_WritePinOutput(GPIOB, 22, 0);
+			GPIO_WritePinOutput(GPIOE, 26, 1);
+			break;
+		case GREEN:
+			GPIO_WritePinOutput(GPIOB, 21, 1);
+			GPIO_WritePinOutput(GPIOB, 22, 1);
+			GPIO_WritePinOutput(GPIOE, 26, 0);
+			break;
+		case BLUE:
+			GPIO_WritePinOutput(GPIOB, 21, 0);
+			GPIO_WritePinOutput(GPIOB, 22, 1);
+			GPIO_WritePinOutput(GPIOE, 26, 1);
+			break;
+		}
 	}
 	return 0;
 }
